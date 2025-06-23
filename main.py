8c881657faec2bd113c76f34a45b3be267bbd5c2
yagneshreddy-somavarapu <yagneshreddysomavarapu@gmail.com>
2025-06-23 23:14:45 +0530

    5th with database

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import pymongo
+from pydantic import BaseModel
+
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
@@ -8,6 +11,23 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+mycli = pymongo.MongoClient("mongodb+srv://yagneshreddysomavarapu:rPI7gE3pjVXWpca6@test.4injoid.mongodb.net/")
+mydb = mycli["test"]
+mycol = mydb["test"]
+
+class email_data(BaseModel):
+   email: str
+   password : str
+
 @app.get("/")
 def read_root():
     return {"message": "Hello from FastAPI on Fly.io"}
+@app.post("/email")
+def email_det(data:email_data):
+    data = {
+        "email":data.email,
+        "password":data.password
+      }
+    mycol.insert_one(data)
+    return "sucessfuly submited"
